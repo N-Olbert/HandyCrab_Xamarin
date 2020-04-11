cd64f0f89f57f104d4bceab1d3633e383d98dcc5
JulianZepf <i17046@hb.dhbw-stuttgart.de>
2020-04-11 10:59:21 +0200

    Added Entities for Barrier and Solution

---
+++ HandyCrab/HandyCrab.Common/Entitys/Barrier.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace HandyCrab.Common.Entitys
+{
+    class Barrier
+    {
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("userId")]
+        public string UserId { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("longitude")]
+        public double Longitude { get; set; }
+
+        [JsonProperty("latitude")]
+        public double Latitude { get; set; }
+
+        [JsonProperty("picture")]
+        public string Picture { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("postcode")]
+        public string Postcode { get; set; }
+
+        [JsonProperty("solution")]
+        public Solution[] Solution { get; set; }
+
+        [JsonProperty("upvotes")]
+        public int Upvotes { get; set; }
+
+        [JsonProperty("downvotes")]
+        public int Downvotes { get; set; }
+
+        [JsonProperty("vote")]
+        public int Vote { get; set; }
+    }
+}
---
+++ HandyCrab/HandyCrab.Common/Entitys/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace HandyCrab.Common.Entitys
+{
+    public class Solution
+    {
+        [JsonProperty("_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("userId")]
+        public string UserId { get; set; }
+
+        [JsonProperty("upvotes")]
+        public int Upvotes { get; set; }
+
+        [JsonProperty("downvotes")]
+        public int Downvotes { get; set; }
+
+        [JsonProperty("vote")]
+        public int Vote { get; set; }
+    }
+}
