0978c307a850b3a911321686b5f4a47b5be83b46
JulianZepf <i17046@hb.dhbw-stuttgart.de>
2020-04-17 13:41:52 +0200

    Added tests for LoginViewModel and RegisterViewModel

---
+++ HandyCrab/HandyCrab.Business.Tests/LoginViewModelTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using FakeItEasy;
+using HandyCrab.Business.Services;
+using HandyCrab.Business.ViewModels;
+using HandyCrab.Common.Entitys;
+using HandyCrab.Business.Tests.TestBusiness;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HandyCrab.Business.Tests
+{
+    [TestClass]
+    public class LoginViewModelTests
+    {
+        [ScopedTestMethod]
+        public void LoginSuccessful()
+        {
+            //Arrange
+            var fake = A.Fake<ILoginClient>();
+
+            A.CallTo(() => fake.LoginAsync(A<Login>._)).Returns(new Failable<User>(new User(), System.Net.HttpStatusCode.OK));
+
+            TestScope.Context.ReplaceFactoryInstance(typeof(ILoginClient), fake);
+
+            var instance = new LoginViewModel();
+            instance.UserName = "testUsername";
+            instance.Password = "testPassword";
+
+            Assert.IsTrue(instance.LoginCommand.CanExecute(null));
+            Assert.IsTrue(instance.IsUserNameValid);
+            Assert.IsTrue(instance.IsPasswordValid);
+
+            bool succeeded = false;
+            instance.LoginSucceeded += (sender, args) => succeeded = true;
+            instance.LoginCommand.Execute(null);
+
+            Assert.IsTrue(succeeded);
+        }
+        [ScopedTestMethod]
+        public void LoginRejected()
+        {
+            //Arrange
+            var fake = A.Fake<ILoginClient>();
+
+            A.CallTo(() => fake.LoginAsync(A<Login>._)).Returns(new Failable<User>(new Exception()));
+
+            TestScope.Context.ReplaceFactoryInstance(typeof(ILoginClient), fake);
+
+            var instance = new LoginViewModel();
+            instance.UserName = "testUsername";
+            instance.Password = "testPassword";
+
+            Assert.IsTrue(instance.LoginCommand.CanExecute(null));
+            Assert.IsTrue(instance.IsUserNameValid);
+            Assert.IsTrue(instance.IsPasswordValid);
+
+            bool rejected = false;
+            instance.LoginRejected += (sender, args) => rejected = true;
+            instance.LoginCommand.Execute(null);
+
+            Assert.IsTrue(rejected);
+        }
+        [ScopedTestMethod]
+        public void CannotTriggerLoginWithoutUsernameOrPassword()
+        {
+            var instance = new LoginViewModel();
+
+            Assert.IsTrue(!instance.LoginCommand.CanExecute(null));
+
+            instance.UserName = "testUsername";
+
+            Assert.IsTrue(!instance.LoginCommand.CanExecute(null));
+
+            instance.UserName = "";
+            instance.Password = "testPassword";
+
+            Assert.IsTrue(!instance.LoginCommand.CanExecute(null));
+
+            instance.UserName = "testUsername";
+
+            Assert.IsTrue(instance.LoginCommand.CanExecute(null));
+        }
+    }
+}
---
+++ HandyCrab/HandyCrab.Business.Tests/RegisterViewModelTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using FakeItEasy;
+using HandyCrab.Business.Services;
+using HandyCrab.Business.ViewModels;
+using HandyCrab.Common.Entitys;
+using HandyCrab.Business.Tests.TestBusiness;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HandyCrab.Business.Tests
+{
+    [TestClass]
+    public class RegisterViewModelTests
+    {
+        [ScopedTestMethod]
+        public void RegisterSuccessful()
+        {
+            //Arrange
+            var fake = A.Fake<IRegisterClient>();
+
+            A.CallTo(() => fake.RegisterAsync(A<UserWithPassword>._)).Returns(new Failable<User>(new User(), System.Net.HttpStatusCode.OK));
+
+            TestScope.Context.ReplaceFactoryInstance(typeof(IRegisterClient), fake);
+
+            var instance = new RegisterViewModel();
+            instance.UserName = "testUsername";
+            instance.Email = "test@email.com";
+            instance.Password = "testPassword";
+
+            Assert.IsTrue(instance.LoginCommand.CanExecute(null));
+            Assert.IsTrue(instance.IsUserNameValid);
+            Assert.IsTrue(instance.IsEmailValid);
+            Assert.IsTrue(instance.IsPasswordValid);
+
+            bool succeeded = false;
+            instance.RegisterSucceeded += (sender, args) => succeeded = true;
+            instance.RegisterCommand.Execute(null);
+
+            Assert.IsTrue(succeeded);
+        }
+
+        [ScopedTestMethod]
+        public void RegisterRejected()
+        {
+            //Arrange
+            var fake = A.Fake<IRegisterClient>();
+
+            A.CallTo(() => fake.RegisterAsync(A<UserWithPassword>._)).Returns(new Failable<User>(new Exception()));
+
+            TestScope.Context.ReplaceFactoryInstance(typeof(IRegisterClient), fake);
+
+            var instance = new RegisterViewModel();
+            instance.UserName = "testUsername";
+            instance.Email = "test@email.com";
+            instance.Password = "testPassword";
+
+            Assert.IsTrue(instance.LoginCommand.CanExecute(null));
+            Assert.IsTrue(instance.IsUserNameValid);
+            Assert.IsTrue(instance.IsEmailValid);
+            Assert.IsTrue(instance.IsPasswordValid);
+
+            bool rejected = false;
+            instance.RegisterRejected += (sender, args) => rejected = true;
+            instance.RegisterCommand.Execute(null);
+
+            Assert.IsTrue(rejected);
+        }
+
+        [ScopedTestMethod]
+        public void CannotTriggerRegisterWithoutUsernameEmailOrPassword()
+        {
+            var instance = new RegisterViewModel();
+
+            Assert.IsTrue(!instance.RegisterCommand.CanExecute(null));
+
+            instance.UserName = "testUsername";
+
+            Assert.IsTrue(!instance.RegisterCommand.CanExecute(null));
+
+            instance.UserName = "";
+            instance.Password = "testPassword";
+
+            Assert.IsTrue(!instance.RegisterCommand.CanExecute(null));
+
+            instance.Password = "";
+            instance.Email = "test@email.com";
+
+            Assert.IsTrue(!instance.RegisterCommand.CanExecute(null));
+
+            instance.UserName = "testUsername";
+            instance.Password = "testPassword";
+            instance.Email = "";
+
+            Assert.IsTrue(!instance.RegisterCommand.CanExecute(null));
+
+            instance.Password = "";
+            instance.Email = "test@email.com";
+
+            Assert.IsTrue(!instance.RegisterCommand.CanExecute(null));
+
+            instance.UserName = "";
+            instance.Password = "testPassword";
+
+            Assert.IsTrue(!instance.RegisterCommand.CanExecute(null));
+
+            instance.UserName = "testUsername";
+
+            Assert.IsTrue(instance.LoginCommand.CanExecute(null));
+        }
+    }
+}
