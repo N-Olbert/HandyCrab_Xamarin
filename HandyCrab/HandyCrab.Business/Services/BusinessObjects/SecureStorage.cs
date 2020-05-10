d5e2ee9854c3fc0f3484f70fbbc9ff6835fe3f86
N-Olbert <i17026@hb.dhbw-stuttgart.de>
2020-05-10 12:48:37 +0200

    App works now on IOS (tested in emulator)

--- HandyCrab/HandyCrab.Business/Services/BusinessObjects/SecureStorage.cs
+++ HandyCrab/HandyCrab.Business/Services/BusinessObjects/SecureStorage.cs
@@ -1,17 +1,34 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
 
 namespace HandyCrab.Business.Services.BusinessObjects
 {
     public class SecureStorage : ISecureStorage
     {
+        private static readonly Lazy<ConcurrentDictionary<string, string>> iOSEmulatorStorageInstance = new Lazy<ConcurrentDictionary<string, string>>();
+        private static ConcurrentDictionary<string, string> iOSEmulatorStorage => iOSEmulatorStorageInstance.Value;
         public async Task StoreAsync(string key, string value)
         {
+            if(Xamarin.Essentials.DeviceInfo.Platform == Xamarin.Essentials.DevicePlatform.iOS &&
+               Xamarin.Essentials.DeviceInfo.DeviceType == Xamarin.Essentials.DeviceType.Virtual)
+            {
+                iOSEmulatorStorage.AddOrUpdate(key, value, (currentKey, oldValue) => value);
+                return;
+            }
+
             await Xamarin.Essentials.SecureStorage.SetAsync(key, value);
         }
 
         public async Task<string> GetAsync(string key)
         {
+            if (Xamarin.Essentials.DeviceInfo.Platform == Xamarin.Essentials.DevicePlatform.iOS &&
+                Xamarin.Essentials.DeviceInfo.DeviceType == Xamarin.Essentials.DeviceType.Virtual)
+            {
+                iOSEmulatorStorage.TryGetValue(key, out var value);
+                return value;
+            }
+
             return await Xamarin.Essentials.SecureStorage.GetAsync(key);
         }
     }
